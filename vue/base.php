8dc18f48d82576735e68229dc9fa88f7935bba61
matthieuthuetdev <mthuet.pro@gmail.com>
2024-10-20 14:40:26 +0200

    crate the first menu and the home page end the ruls page

--- vue/base.php
+++ vue/base.php
@@ -10,20 +10,21 @@
 </head>
 
 <body>
-  <nav class="navbar navbar-expand-lg navbar-light bg-light">
-    <a class="navbar-brand" href="#">Navbar</a>
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse bg-black" id="navbarNavAltMarkup">
-      <div class="navbar-nav ">
-        <a class="nav-item nav-link active " href="#">Home </a>
-        <a class="nav-item nav-link text-white " href="#">Features</a>
-        <a class="nav-item nav-link text-white " href="#">Pricing</a>
-        <a class="nav-item nav-link disabled  text-white" href="#">Disabled</a>
+  <header>
+    <nav class="navbar navbar-expand-lg navbar-light bg-dark ">
+      <a class="navbar-brand text-white" href="index.php?p=home">Short blind test</a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse bg-dark" id="navbarNavAltMarkup">
+        <div class="navbar-nav ">
+          <a class="nav-item nav-link text-white active " href="index.php?p=home">Home </a>
+          <a class="nav-item nav-link text-white " href="index.php?p=rules">Règle du jeu</a>
+          <a class="nav-item nav-link text-white " href="index.php?p=signUp">S'inscrir</a>
+          <a class="nav-item nav-link text-white " href="index.php?p=signIn">Se connecter</a>
+        </div>
       </div>
-    </div>
-  </nav>
+    </nav>
 
   </header>
   <?php echo $content; ?>
---
+++ vue/home.php
@@ -0,0 +1,7 @@
+<h1>Accueil</h1>
+<p>
+    Bienvenue sur le site du jeu Short Blind Test ! Le concept est simple : chaque jour, à une heure précise, un extrait d'une chanson d'une seconde sera disponible. À vous de deviner de quelle chanson il s'agit !<br>
+    Pour en savoir plus sur les règles du jeu et participer, rendez-vous sur la page des <a href="index.php?p=regle">règles</a>.
+</p>
+<h2>Comment se déroule le jeu ?</h2>
+<p>Une partie de jeu dure 1 mois et les points sont comptés ! Si vous trouvez l'artiste, vous avez un point et si vous trouvez la chanson, vous avez aussi un point. Au cours de la partie, des bonus seront disponibles, par exemple : l'aide d'un ami, le droit d'écouter un autre extrait d'une seconde, des points doubles pendant 24 heures, etc. Ces bonus seront achetables avec des points que vous obtiendrez en gagnant des parties ou en jouant à des mini-jeux qui seront créés par la suite.</p>
