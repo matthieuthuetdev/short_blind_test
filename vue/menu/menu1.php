1d4215baa725be5056b86ffbe31d57fea6dc6b48
MatthieuThuet <98526173+matthieuthuetdev@users.noreply.github.com>
2024-12-13 15:57:54 +0100

    push

--- vue/menu/menu1.php
+++ vue/menu/menu1.php
@@ -1,4 +1,4 @@
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
+<nav class="navbar navbar-expand-lg navbar-light bg-dark">
   <a class="navbar-brand" href="#"></a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
@@ -6,26 +6,19 @@
 
   <div class="collapse navbar-collapse" id="navbarSupportedContent">
     <ul class="navbar-nav mr-auto">
-      <li class="nav-item active">
-        <a class="nav-link" href="#">Accueil<span class="sr-only">(current)</span></a>
+      <li class="nav-item active ">
+        <a class="nav-link text-white" href="#">Accueil<span class="sr-only">(current)</span></a>
       </li>
       <li class="nav-item">
-        <a class="nav-link" href="#">règle du jeu</a>
-      </li>
-      <li class="nav-item dropdown">
-        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-          Dropdown
-        </a>
-        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-          <a class="dropdown-item" href="#">Action</a>
-          <a class="dropdown-item" href="#">Another action</a>
-          <div class="dropdown-divider"></div>
-          <a class="dropdown-item" href="#">Something else here</a>
-        </div>
+        <a class="nav-link text-white" href="index.php?p=rules">règle du jeu</a>
       </li>
       <li class="nav-item">
-        <a class="nav-link disabled" href="#">Disabled</a>
+        <a class="nav-link text-white" href="index.php?p=signIn">Se connecter</a>
       </li>
+      <li class="nav-item">
+        <a class="nav-link text-white" href="index.php?p=signUp">S'inscrire</a>
+      </li>
+
     </ul>
   </div>
 </nav>
